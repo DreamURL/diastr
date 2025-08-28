2a35856972e732e80332145b4c3a4f85e1272c7f
minsun1231 <oodework@gmail.com>
2025-08-28 19:16:25 +0900

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
     domains: ['localhost'],
     unoptimized: true
-  }
+  },
 }
 
 module.exports = nextConfig
